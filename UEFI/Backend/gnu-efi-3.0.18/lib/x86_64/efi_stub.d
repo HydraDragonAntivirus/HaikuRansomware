17182814c14823f3a8355d7f50740702750ec570
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-10-22 20:55:28 +0300

    Creating new branch

--- UEFI/Backend/gnu-efi-3.0.18/lib/x86_64/efi_stub.d
+++
@@ -1,2 +0,0 @@
-C:\Users\victim\Documents\GitHub\HaikuRansomware\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\efi_stub.o: \
- C:\Users\victim\Documents\GitHub\HaikuRansomware\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\efi_stub.S
